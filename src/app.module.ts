4e4a17c2d1c5f2415883cdb4ea2b644912d0df9c
heydrigh <heydrigh@gmail.com>
2022-12-10 15:01:09 -0300

    fix: add bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig(__dirname)),
+    UsersModule,
   ],
 })
 export class AppModule {}
